4f3c7e105af6b603ec4cdff8c601c8a1cc8b4e81
Jonas Nicklas <jonas.nicklas@gmail.com>
2011-11-04 17:03:36 +0100

    Styling

--- todos/app/assets/javascripts/todo_template.js
+++ todos/app/assets/javascripts/todo_template.js
@@ -1,8 +1,8 @@
 var TodoTemplate = function(todo) {
   return todo.description +
   ' <a href="#" class="edit">Edit</a>' +
-  '<form><label>Description<br/>' +
+  '<form class="form-stacked"><label>Description<br/>' +
   '<input type="text" name="description" ' +
   'value="' + todo.description + '"/>' +
-  '<input type="submit" value="Save"></form>';
+  '<input type="submit" class="btn primary" value="Save"></form>';
 };
